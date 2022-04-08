15dcef943e200c2f2dc331f7d69640dfddba2974
Alejandrodlk <ale_165851@hotmail.com>
2022-04-08 10:51:57 -0300

    estructura inicial develop

--- museumSports/app.js
+++ museumSports/app.js
@@ -17,10 +17,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/* app.use('/', indexRouter);
+app.use('/users', usersRouter); */
+
+app.get("/" , (req,res) => res.sendFile(path.resolve(__dirname , "views","index.html")))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
