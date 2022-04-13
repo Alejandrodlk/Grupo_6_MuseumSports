112caf35dfd5bc16c933c23a0d45c171b3684b18
Emiliano-dos-Reis <edosreis26@gmail.com>
2022-04-13 18:44:29 -0300

    register

--- museumSports/app.js
+++ museumSports/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter); */
 
 app.get("/" , (req,res) => res.sendFile(path.resolve(__dirname , "views","index.html")))
-
+app.get("/register" , (req,res) => res.sendFile(path.resolve(__dirname , "views","register.html")))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
