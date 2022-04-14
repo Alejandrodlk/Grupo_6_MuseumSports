11c6f9d8a993a00e4add7dc6effdfdda79bfb4d5
unknown <fmyondaime2@gmail.com>
2022-04-13 23:28:01 -0300

    header creado con menu hamburguesa

--- museumSports/public/javascript/burguer.js
+++ museumSports/public/javascript/burguer.js
@@ -1,6 +1,10 @@
 let burguer = document.querySelector('.main-burguer');
 let nav_menu = document.querySelector('.nav-bars-container');
+let menuClose = document.querySelector('.main-burguerClose')
 
-burguer.addEventListener("click", () => {
-    nav_menu.classList.toggle("is-active")
+burguer.addEventListener('click', () => {
+    nav_menu.classList.add("active")
+})
+menuClose.addEventListener('click', () => {
+    nav_menu.classList.remove('active')
 })
