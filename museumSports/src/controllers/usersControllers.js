b1dea2a7587f59c00045d858390bf82ad2590249
unknown <fmyondaime2@gmail.com>
2022-05-30 18:43:48 -0300

    Agrego el user.json con el preoceso de registrar usuarios

--- museumSports/src/controllers/usersControllers.js
+++ museumSports/src/controllers/usersControllers.js
@@ -1,10 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+
+/* const readJSON = JSON.parse(fs.readFileSync("src/data/products.json" ,"utf8")) */
+
+const readJSON = () => {
+    const users = JSON.parse(fs.readFileSync("src/data/users.json", 'utf-8'));	
+	return users
+}
+
+const saveJSON = (e) => fs.writeFileSync("src/data/users.json" , JSON.stringify(e,null,3))
+
 module.exports = {
 
     register : (req,res) => res.render("register"),
-
-    login : (req,res) => res.render("login"),
-
     
+    processRegister: (req, res) => {
+        let users = readJSON();
+        const {id, name, lastname, image, category, password, email} = req.body
 
+        const newUser = {
+            id: users[users.length - 1].id +1,
+            name: name.trim(),
+            lastname: lastname.trim(),
+            email ,
+            password ,
+            image: "foto.jpg",
+            category
+        };
 
+        users.push(newUser);
+
+        saveJSON(users)
+
+        res.redirect('/')
+    }, 
+
+    login : (req,res) => res.render("login")
+
+   
 }
--- museumSports/src/routes/users.js
+++ museumSports/src/routes/users.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 
-const {login , register } = require("../controllers/usersControllers")
+const {login , register, processRegister } = require("../controllers/usersControllers")
 
 /* /users. */
 router.get('/login', login )
 
 router.get("/register" , register )
 
-
+router.post('/register', processRegister)
 
 
 module.exports = router;
