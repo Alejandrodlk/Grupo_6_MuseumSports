9f528f3f5f52b694acd4b37ec665cb5c0bdfc8e8
Emiliano-dos-Reis <edosreis26@gmail.com>
2022-08-15 21:51:11 -0300

    f-e productAdd

---
+++ museumSports/public/javascript/productAdd.js
@@ -0,0 +1,97 @@
+console.log('productAdd.js success');
+
+const $ = (element) => document.getElementById (element);
+const regExLetter = /^[A-Z]+$/i;
+const regExEmail =  /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
+const regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+
+
+
+
+$('title').addEventListener('blur', function(){
+
+    switch (true) {
+        case !this.value.trim():
+            $('errorNombre').innerHTML = "Debes ingresar el nombre del producto";
+            this.classList.add('is-invalid')
+            break;
+        case this.value.trim().length < 15 || this.value.trim().length > 255 :
+            $('errorNombre').innerHTML = "El nombre del producto debe tener como mínimo 15 caracteres";
+            this.classList.add('is-invalid')
+            break
+        default:
+            this.classList.remove('is-invalid')
+            this.classList.add('is-valid')
+            $('errorNombre').innerHTML = null;
+            break;
+    }
+});
+
+
+$('description').addEventListener('blur', function(){
+
+    switch (true) {
+        case !this.value.trim():
+            $('errorDescripción').innerHTML = "Debes ingresar una descripción del producto";
+            this.classList.add('is-invalid')
+            break;
+        case this.value.trim().length < 2 || this.value.trim().length > 255 :
+            $('errorDescripción').innerHTML = "La descripción debe tener como mínimo 30 caracteres";
+            this.classList.add('is-invalid')
+            break
+        default:
+            this.classList.remove('is-invalid')
+            this.classList.add('is-valid')
+            $('errorDescripción').innerHTML = null;
+            break;
+    }
+});
+
+
+$('price').addEventListener('blur', function(){
+
+    switch (true) {
+        case !this.value.trim():
+            $('errorPrecio').innerHTML = "Debes ingresar el precio del producto";
+            this.classList.add('is-invalid')
+            break;
+        default:
+            this.classList.remove('is-invalid')
+            this.classList.add('is-valid')
+            $('errorPrecio').innerHTML = null;
+            break;
+    }
+});
+
+$('discount').addEventListener('blur', function(){
+
+    switch (true) {
+        case !this.value.trim():
+            $('errorDescuento').innerHTML = "Debes ingresar el descuento % del producto";
+            this.classList.add('is-invalid')
+            break;
+        default:
+            this.classList.remove('is-invalid')
+            this.classList.add('is-valid')
+            $('errorDescuento').innerHTML = null;
+            break;
+    }
+});
+
+
+$('price').addEventListener('blur', function(){
+
+    switch (true) {
+        case !this.value.trim():
+            $('errorPrecio').innerHTML = "Debes ingresar el precio del producto";
+            this.classList.add('is-invalid')
+            break;
+        default:
+            this.classList.remove('is-invalid')
+            this.classList.add('is-valid')
+            $('errorPrecio').innerHTML = null;
+            break;
+    }
+});
+
+
